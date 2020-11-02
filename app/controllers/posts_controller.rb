668cf4b903888011f839e82076a8c24d250baecc
Wh1te-7 <ohas7cancer@icloud.com>
2020-11-02 12:15:42 +0900

    Ajaxappの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class Postscontroller < ApplicationController
 
   def index
-    @posts = Posts.all
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(memo:params[:memo]）
+    redirect_to action: :index
   end
 end
